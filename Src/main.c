156934d000bdaaa1d27aac82538d80afa0ea85af
Paul Czeresko <p.czeresko.3@gmail.com>
2019-11-01 11:21:57 -0400

    Configure and initialize IP in main

--- Src/main.c
+++ Src/main.c
@@ -27,7 +27,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "FreeRTOS_IP.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,6 +48,21 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+
+/* The MAC address array is not declared const as the MAC address will
+normally be read from an EEPROM and not hard coded (in real deployed
+applications).*/
+uint8_t ucMACAddress[ 6 ] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55 };
+
+/* Define the network addressing.  These parameters will be used if either
+ipconfigUDE_DHCP is 0 or if ipconfigUSE_DHCP is 1 but DHCP auto configuration
+failed. */
+static const uint8_t ucIPAddress[ 4 ] = { 192, 168, 2, 12 };
+static const uint8_t ucNetMask[ 4 ] = { 255, 255, 255, 255 };
+static const uint8_t ucGatewayAddress[ 4 ] = { 192, 168, 2, 1 };
+
+/* The following is the address of an OpenDNS server. */
+static const uint8_t ucDNSServerAddress[ 4 ] = { 8, 8, 8, 8 };
 
 /* USER CODE END PV */
 
@@ -101,7 +116,11 @@ int main(void)
 	MX_ETH_Init();
 	MX_RNG_Init();
 	/* USER CODE BEGIN 2 */
-
+	FreeRTOS_IPInit( ucIPAddress,
+			ucNetMask,
+			ucGatewayAddress,
+			ucDNSServerAddress,
+			ucMACAddress );
 	/* USER CODE END 2 */
 
 	/* Call init function for freertos objects (in freertos.c) */
